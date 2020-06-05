0078c3fc3e6d18b274a68d4addadb2df43b96b4a
Dylan Kieft <dylankieft@live.nl>
2020-06-05 17:08:03 +0200

    changed python version of flask. added mysql connection

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,39 @@
 # coding=utf-8
 
 from flask import render_template, flash, redirect, session, url_for, request, g, Markup
+from flaskext.mysql import MySQL
 from app import app
-#from app import user
+
+
+# TODO: needs to be moved from here.
+#       Might be safer in the run.py
+#       However, setting the config 
+#       details there does not same to work.
+def sql():
+    app.config['MYSQL_DATABASE_USER'] = 'user'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+    app.config['MYSQL_DATABASE_DB'] = 'db'
+    app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+    return MySQL(app)
+
+mysql = sql()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    from flaskext.mysql import MySQL
-
-    # mysql = MySQL()
-    # app.config['MYSQL_DATABASE_USER'] = 'root'
-    # app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-    # app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-    # app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-    # mysql.init_app(app)
-
     return render_template('login.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
-
+# Test
+@app.route('/db')
+def db():
+    con = mysql.connect()
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO test VALUES (1)")
+    con.commit()
+    con.close()
+    return "db added"
 
