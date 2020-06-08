5a29355bc270d0af6374719db80dfbb31768c188
Auke Schuringa <auke.schuringa@student.uva.nl>
2020-06-08 08:35:08 -0400

    Fix use of env variables.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import os
+
+from app.api import register_central, register_data
+from app.database import init_mysql
+from app.main import blueprint as main_routes
 
 app = Flask(__name__)
-# app.config.from_object('config')
-from app import routes
+app.config.from_object('config')
+app.register_blueprint(main_routes, url_prefix='/')
+init_mysql(app)
 
+if os.environ['FLASK_SERVER_TYPE'].upper() == 'CENTRAL':
+    register_central(app)
+elif os.environ['FLASK_SERVER_TYPE'].upper() == 'DATA':
+    register_data(app)
+# else:
+    # Loads central if not specified or wrong
+    # register_central(app)
--- run.py
+++ run.py
@@ -1,4 +1,22 @@
-from app import app
+import argparse
+import os
+
+
+def main(server: str=None, port: int=None):
+    if 'FLASK_SERVER_TYPE' not in os.environ:
+        os.environ['FLASK_SERVER_TYPE'] = server.upper()
+    if 'FLASK_PORT' in os.environ:
+        port = os.environ['FLASK_PORT']
+    from app import app as application
+    application.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--server', type=str,
+                        required='SERVER_TYPE' in os.environ,
+                        help='Server type. Can be \'central\' or \'data\'.')
+    parser.add_argument('-p', '--port', type=int, default=1337,
+                        help='Port to use. Default is 1337')
+    args = parser.parse_args()
+    main(args.server, args.port)
+
