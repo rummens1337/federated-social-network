d341aa9ec9211321bf64263f5da097965515fe33
Nick Moone <nick.moone98@gmail.com>
2020-06-10 20:11:17 +0200

    Added JSON request function.

--- app/static/javascript/test.js
+++ app/static/javascript/test.js
@@ -1,20 +1,33 @@
+// GET or POST JSON from url and apply it to func.
+// Params:
+// - method: the method to use ('GET'/'POST').
+// - url: the url to do the request to.
+// - func: the function to apply the JSON data to.
+function requestJSON(method, url, func) {
+    $.ajax({
+        type: method,
+        url: url,
+        crossDomain: true,
+        success: func(data)
+    });
+}
+
 // Load usernames to recent friends list.
 $( document ).ready(function (){
-    // Get usernames div.
-    var $usernames = $('#usernames');
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:9000/api/user/',
+        crossDomain: true,
 
         success: function(json) {
 
             // Uncomment for debugging.
             // alert(json)
+            
+            // Get usernames div.
+            var $usernames = $('#usernames');
 
-            obj = JSON.parse(json)
-
-            for (i in obj.data.usernames) {
+            for (i in json.data.usernames) {
                 $usernames.append('<li>username: '+ obj.data.usernames[i] + '</li>');
             }
         }
@@ -23,9 +36,6 @@ $( document ).ready(function (){
 
 // Load post_ids to recent posts list.
 $( document ).ready(function (){
-    // Get posts div.
-    var $usernames = $('#usernames');
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:9000/api/user/posts?username=user1',
@@ -34,12 +44,14 @@ $( document ).ready(function (){
 
             // Uncomment for debugging.
             alert(json)
+            
+            // Get posts div.
+            var $usernames = $('#usernames');
 
-            obj = JSON.parse(json)
-
-            for (i in obj.data.posts) {
+            for (i in json.data.posts) {
                 $usernames.append('<li>username: '+ obj.data.usernames[i] + '</li>');
             }
         }
     });
 });
+
