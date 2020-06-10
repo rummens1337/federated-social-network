bbaddae2d0ff9e3cc31c0ebcb6d5698104fb1d24
Nick Moone <nick.moone98@gmail.com>
2020-06-10 16:24:30 +0200

    Added standard ajax function.

--- app/static/javascript/test.js
+++ app/static/javascript/test.js
@@ -1,8 +1,19 @@
+// GET or POST JSON from url and apply it to func.
+// Params:
+// - method: the method to use ('GET'/'POST').
+// - url: the url to do the request to.
+// - func: the function to apply the JSON data to.
+function getJSON(method, url, func) {
+    $.ajax({
+        type: method,
+        url: url,
+        crossDomain: true,
+        success: func(data)
+    });
+}
+
 // Load usernames to recent friends list.
 $( document ).ready(function (){
-    // Get usernames div.
-    var $usernames = $('#usernames');
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:9000/api/user/',
@@ -12,6 +23,9 @@ $( document ).ready(function (){
 
             // Uncomment for debugging.
             // alert(json)
+            
+            // Get usernames div.
+            var $usernames = $('#usernames');
 
             for (i in json.data.usernames) {
                 $usernames.append('<li>username: '+ obj.data.usernames[i] + '</li>');
@@ -22,9 +36,6 @@ $( document ).ready(function (){
 
 // Load post_ids to recent posts list.
 $( document ).ready(function (){
-    // Get posts div.
-    var $usernames = $('#usernames');
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:9000/api/user/posts?username=user1',
@@ -33,6 +44,9 @@ $( document ).ready(function (){
 
             // Uncomment for debugging.
             alert(json)
+            
+            // Get posts div.
+            var $usernames = $('#usernames');
 
             for (i in json.data.posts) {
                 $usernames.append('<li>username: '+ obj.data.usernames[i] + '</li>');
