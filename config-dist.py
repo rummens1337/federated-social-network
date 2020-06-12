7732890f3c2732f42d3b8876a16c12578ca17e29
Danny Opdam <danny.opdam@hotmail.com>
2020-06-12 12:49:18 +0200

    Comments breken je hele docker

--- config-dist.py
+++ config-dist.py
@@ -2,7 +2,7 @@
 # If u change them later remove the 
 # mysql docker volumes (make rm) and restart (make run).
 
-# MYSQL_DATABASE_USER = 'user'
-# MYSQL_DATABASE_PASSWORD = 'password'
+MYSQL_DATABASE_USER = 'user'
+MYSQL_DATABASE_PASSWORD = 'password'
 MYSQL_DATABASE_DB = 'db'
 MYSQL_DATABASE_HOST = 'mysql'
