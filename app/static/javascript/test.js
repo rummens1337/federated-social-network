477d879f46356ce137faeff779fb55619ae2b3ca
Nick Moone <nick.moone98@gmail.com>
2020-06-12 12:15:01 +0200

    Removed alerts.

--- app/static/javascript/test.js
+++ app/static/javascript/test.js
@@ -1,35 +1,6 @@
-// URL of servers ENDING WITH '/'
-// centralServer is hardcoded
-// dataServer is requested using function getDataServer
-var centralServer = 'http://localhost:5000/'
-var dataServer = 'http://localhost:9000/';
-
-// User variables.
-var username = 'testuser';
-
-// GET or POST JSON from url and apply it to func.
-// Params:
-// - type: the request type to use ('GET'/'POST').
-// - url: the url to do the request to.
-// - func: the function to apply the JSON data to.
-function requestJSON(type, url, func) {
-    $.ajax({
-        type: type,
-        url: url,
-        crossDomain: true,
-        success: func
-    });
-};
-
-// Get address of data server for this user from the central server.
-var getDataServer = function(req) {
-    dataServer = req.data.address;
-};
-
 var applyUsernames = function(req) {
     // Get usernames div.
     var $usernames = $('#usernames');
-
     for (i in req.data.usernames) {
         $usernames.append('<li>username: '+ req.data.usernames[i] + '</li>');
     }
@@ -45,7 +16,6 @@ var applyPosts = function(req) {
 };
 
 $(document).ready(function() {
-    requestJSON('GET', centralServer + 'api/user/address?username=' + username, getDataServer);
     if (dataServer == '') {
         // Set dataserver for development.
         dataServer = 'http://localhost:9000/'
