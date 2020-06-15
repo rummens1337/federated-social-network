3c0167d25562878e663b5dfb7e5320ca8a91c6b6
Dylan Kieft <dylankieft@live.nl>
2020-06-15 20:45:51 +0200

    start to connection between login frontend and backend

--- app/static/javascript/dataHandler.js
+++ app/static/javascript/dataHandler.js
@@ -1,31 +1,21 @@
 // URL of servers ENDING WITH '/'
 // centralServer is hardcoded
 // dataServer is requested using function getDataServer
-var centralServer = 'http://localhost:5000/'
-var dataServer = 'http://localhost:9000/';
-
-// User variables.
-var username = 'testuser';
+// var centralServer = 'http://localhost:5000/'
+// var dataServer = 'http://localhost:9000/';
 
 // GET or POST JSON from url and apply it to func.
 // Params:
 // - type: the request type to use ('GET'/'POST').
 // - url: the url to do the request to.
 // - func: the function to apply the JSON data to.
-function requestJSON(type, url, func) {
+function requestJSON(type, url, data=null, success=null, error=null) {
     $.ajax({
         type: type,
         url: url,
+        data: data,
         crossDomain: true,
-        success: func
+        success: success,
+        error: error
     });
 };
-
-// Get address of data server for this user from the central server.
-var getDataServer = function(req) {
-    dataServer = req.data.address;
-};
-
-$(document).ready(function() {
-    requestJSON('GET', centralServer + 'api/user/address?username=' + username, getDataServer);
-});
