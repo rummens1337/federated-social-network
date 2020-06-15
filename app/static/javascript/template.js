48125bfb8ae1bd138af67cd8f65725d14b8eb6cb
Danny Opdam <danny.opdam@hotmail.com>
2020-06-15 18:19:38 +0200

    Added opaque header when menu open

--- app/static/javascript/template.js
+++ app/static/javascript/template.js
@@ -8,9 +8,9 @@ function toggleNav(){
 function open() {
     document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").style.marginRight = "250px";
-	document.body.style.backgroundColor = "#364958";
-	document.getElementById("jumbotron").style.backgroundColor = "#364958";
-	document.getElementById("search").style.backgroundColor = "#364958";
+	document.body.style.backgroundColor = "rgba(54, 73, 88, 1)";
+	document.getElementById("jumbotron").style.backgroundColor = "rgba(54, 73, 88, 0.5)";
+	document.getElementById("search").style.backgroundColor = "rgba(54, 73, 88, 0.5)";
 	// document.getElementById("b1").style.backgroundColor = "#3b3b3f" ;
 	// document.getElementById("b2").style.backgroundColor = "#3b3b3f" ;
 	// document.getElementById("b3").style.backgroundColor = "#3b3b3f" ;
@@ -19,13 +19,12 @@ function open() {
 function close() {
     document.getElementById("mySidenav").style.width = "0";
   	document.getElementById("main").style.marginRight = "0";
-  	document.body.style.backgroundColor = "#3b3b3f" ;
-  	document.getElementById("jumbotron").style.backgroundColor = "#364958" ;
-  	document.getElementById("search").style.backgroundColor = "#364958" ;
+  	document.body.style.backgroundColor = "rgba(54, 73, 88, 1)" ;
+  	document.getElementById("jumbotron").style.backgroundColor = "rgba(54, 73, 88, 1)" ;
+  	document.getElementById("search").style.backgroundColor = "rgba(54, 73, 88, 1)" ;
  //  	document.getElementById("b1").style.backgroundColor = "#0b132b" ;
 	// document.getElementById("b2").style.backgroundColor = "#0b132b" ;
 	// document.getElementById("b3").style.backgroundColor = "#0b132b" ;
-
 
 }
 
