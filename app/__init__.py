6eb6099f038561f2f3953d577d7b65ed5191353b
Michel Rummens <michelrummens@hotmail.com>
2020-06-16 22:34:29 +0200

    Add custom messages for JWT errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import os
 
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token
+)
 from app.database import init_mysql
 from app.log import init_logger
-from flask_jwt_extended import JWTManager
 
 init_logger()
 app = Flask(__name__)
@@ -14,15 +15,65 @@ init_mysql(app)
 from app.api import register_central, register_data
 from app.type import get_server_type, ServerType
 
+# TODO is this only needed for data / central? if so: move it inside the IF.
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+jwt = JWTManager(app)
+# Using the expired_token_loader decorator, we will now call
+# this function whenever an expired but otherwise valid access
+# token attempts to access an endpoint
+@jwt.expired_token_loader
+def my_expired_token_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+@jwt.unauthorized_loader
+def my_unauthorized_token_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+@jwt.needs_fresh_token_loader
+def my_needs_fresh_token_loader_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+@jwt.revoked_token_loader
+def my_revoked_token_loader_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+@jwt.user_loader_error_loader
+def my_user_loader_error_loader_callback(expired_token):
+    token_type = expired_token['type']
+    return jsonify({
+        'status': 401,
+        'sub_status': 42,
+        'msg': 'The {} token has expired'.format(token_type)
+    }), 401
+
+
 if get_server_type() == ServerType.CENTRAL:
-    app.config['JWT_TOKEN_LOCATION'] = ['cookies']
     from app.api.central.main import blueprint as main_routes
     register_central(app)
+
 elif get_server_type() == ServerType.DATA:
     from app.api.data.main import blueprint as main_routes
     register_data(app)
-
-jwt = JWTManager(app)
 
 app.register_blueprint(main_routes, url_prefix='/')
 
