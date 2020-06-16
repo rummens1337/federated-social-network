5c4d67ce0b19c5c7eaa4bb246a7c18b5745ebe50
Danny Opdam <danny.opdam@hotmail.com>
2020-06-16 12:16:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+# Set the following two lines.
+# If u change them later remove the 
+# mysql docker volumes (make rm) and restart (make run).
+
+# Default values, but NEED to be set in order for the application to work correctly.
+MYSQL_DATABASE_USER = 'user'
+MYSQL_DATABASE_PASSWORD = 'password'
+MYSQL_DATABASE_DB = 'db'
+MYSQL_DATABASE_HOST = 'mysql'
+
 TEMPLATES_AUTO_RELOAD=True
