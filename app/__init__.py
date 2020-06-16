0ce664f7c29784c0d671ce39f2999db95b115c0b
Dylan Kieft <dylankieft@live.nl>
2020-06-17 00:46:30 +0200

    Made frontend finally work with the data servers

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ from flask import Flask
 from app.database import init_mysql
 from app.log import init_logger
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 init_logger()
 app = Flask(__name__)
 app.config.from_object('config')
 init_mysql(app)
+CORS(app)
 
 from app.api import register_central, register_data
 from app.type import get_server_type, ServerType
