4ebbb2c03bcaa33c1f3b076b5afe9b6a2e3d73fa
Tim Laamers <t.laamers@gmail.com>
2020-06-17 15:46:20 +0200

    On registration a server can be chosen from a dropdown menu, server list obtained through central/server/.

--- app/static/javascript/signup.js
+++ app/static/javascript/signup.js
@@ -31,10 +31,34 @@ function sign_up() {
 
         function register(req) {
           dataServer = mock_address;
-          requestJSON('POST', dataServer + '/api/user/register', $(form).serialize(), signupSucces, signupFailed);
+          requestJSON("POST", dataServer + "/api/user/register", $(form).serialize(), signupSucces, signupFailed);
         }
 
         register(null);
     }
   });
 }
+
+$(document).ready( function() {
+  requestJSON("GET", window.location.origin + "/api/server", null, populateServerSelect, requestError);
+
+  function populateServerSelect(req) {
+    var select = document.getElementById('select_server')
+    var data = req.data.servers;
+    console.log(req);
+
+    for(i in data) {
+      var server_option = document.createElement("option");
+      console.log(i)
+      server_option.value = data[i][1]
+      server_option.textContent = data[i][0]
+
+      select.appendChild(server_option);
+    }
+
+  }
+
+  function requestError(req) {
+    alert("Couldn't load servers...")
+  }
+});
