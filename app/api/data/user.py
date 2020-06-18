fa177c276caace502012b4f9d66a83f7fa1b7f76
Maqsood Ehsan <mqehsan@hotmail.com>
2020-06-18 19:54:36 +0200

    yes

--- app/api/data/user.py
+++ app/api/data/user.py
@@ -217,7 +217,7 @@ def delete():
 
 
 @blueprint.route('/edit', methods=['POST'])
-#@jwt_required
+@jwt_required
 def edit():
     # username = get_jwt_identity()
     username = request.form['username']
