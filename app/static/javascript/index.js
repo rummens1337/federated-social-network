67cd2f045b1178f07f6105b07563bd112155eb84
Dylan Kieft <dylankieft@live.nl>
2020-06-19 00:35:22 +0200

    central server js fix

--- app/static/javascript/index.js
+++ app/static/javascript/index.js
@@ -1,5 +1,3 @@
-var centralServer = window.location.origin;
-
 function setUserSettings(req) {
     document.getElementById('image_url').src = req.data.image_url;
     document.getElementById('location').innerHTML = req.data.location;
@@ -10,9 +8,10 @@ function setUserSettings(req) {
 
 function setDataAddress(req) {
     dataServer = req.data.address;
+    alert( dataServer + '/api/user');
     requestJSON('GET', dataServer + '/api/user', null, setUserSettings, console.log('error called from index.js'));
 }
 
 $(document).ready(function() {
-    requestJSON('GET', centralServer + '/api/user/address', null, setDataAddress, null);
+    requestJSON('GET', '/api/user/address', null, setDataAddress, null);
 });
