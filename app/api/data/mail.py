a16e3f71c6a3678eb7e1ff1041c7052cf457e522
Michel Rummens <michelrummens@hotmail.com>
2020-06-19 16:35:14 +0200

    Working and tested version of email confirmation

--- app/api/data/mail.py
+++ app/api/data/mail.py
@@ -1,43 +1,59 @@
-import os
 import smtplib
-import imghdr
 from email.message import EmailMessage
-from flask import Blueprint, request
-from email.mime.text import MIMEText
+from flask import Blueprint, request, url_for, Flask
+import email, smtplib, ssl
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from flask import current_app
+from app.database import users
+from app.api.utils import good_json_response, bad_json_response
 
-# EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
-# EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 blueprint = Blueprint('data_mail', __name__)
 
-@blueprint.route('/', strict_slashes=False)
-def test():
-    return "test"
-
-@blueprint.route('/verification', strict_slashes=False)
-def send_verification_mail():
-    EMAIL_ADDRESS = "therealfednet@gmail.com"
-    EMAIL_PASSWORD = "fednetbest1"
-
+@blueprint.route('/token/<send_to>', methods=['GET', 'POST'])
+def send_verification_mail(send_to):
+    # Construct message object with receipient and sender
     msg = EmailMessage()
     msg['Subject'] = 'FedNet - Please verify your email!'
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = 'michelrummens@hotmail.com'
+    msg['From'] = current_app.config['EMAIL_ADDRESS']
+    msg['To'] = send_to
 
-    # html = open("app/api/data/simple.html")
-    # msg2 = MIMEText(html.read(), 'html')
+    # Create the secret key based on our little secret :)
+    secret = URLSafeTimedSerializer(current_app.config['EMAIL_SECRET'])
 
-    # msg.set_content(msg2)
+    # Create token based on a user their email and salt to prevent same token.
+    token = secret.dumps(send_to, salt=current_app.config['EMAIL_SALT'])
 
-    msg.add_alternative("""\
-    <!DOCTYPE html>
-    <html>
-        <body>
-            <h1 style="color:SlateGray;">This is an HTML Email!</h1>
-        </body>
-    </html>
-    """, subtype='html')
+    # Create link with token and add it to the body of the mail.
+    link = url_for('data_mail.confirm_email', token=token, _external=True)
+    msg.add_alternative('Your link is {}'.format(link), subtype="plaintext")
 
+    # TODO: add link in template below!
+    # verify_file = open('app/api/data/verify-mail.html')
+    # html = verify_file.read()
+    # msg.add_alternative(html, subtype='html')
 
+    # Connect to the mailserver from google and send the e-mail.
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.login(current_app.config['EMAIL_ADDRESS'], current_app.config['EMAIL_PASSWORD'])
         smtp.send_message(msg)
+    
+    # TODO: If success give user a pop-up in the front-end.  
+    return True
+    # return '<h1>The email you entered is {}. The token is </h1>'.format(token)
+
+@blueprint.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        # Create the secret key based on our little secret :)
+        secret = URLSafeTimedSerializer(current_app.config['EMAIL_SECRET'])
+
+        # Confirm key is in pool and has not expired yet.
+        email = secret.loads(token, salt=current_app.config['EMAIL_SALT'], max_age=3600)
+
+        if users.exists(email=email):
+            users.update({'email_confirmed':1}, email=email)
+            return good_json_response("successfully registered for email " + email)
+        else:
+            return bad_json_response("No user with the email " + email + " exists.")
+    except SignatureExpired:
+        return bad_json_response("The token has expired. Please try registering again.")
