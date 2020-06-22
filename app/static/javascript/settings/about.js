ca6772856142065f36604c89c71a1b95fc0d0640
Coen <coennusse@live.nl>
2020-06-22 11:37:18 +0200

    javascript update

--- app/static/javascript/settings/about.js
+++ app/static/javascript/settings/about.js
@@ -5,8 +5,8 @@ function showHobbies(req) {
     // if (req.data.hobbies.length > 0) {
     if (0 == 1) {
       for (i=0; i < req.data.hobbies.length; i++) {
-          var hobbie = req.data.hobbies[i];
-          showHobbie(hobbie);
+          var hobby = req.data.hobbies[i];
+          showHobby(hobby);
       }
     }
     else {
@@ -14,14 +14,14 @@ function showHobbies(req) {
     }
 }
 
-// This function adds a friend in the div 'hobbie'
-function showHobbie(hobbie) {
-    var content = ' <li class="hobbie-selected"><div class="name">' +
-        hobbie.title + '</div></li>';
+// This function adds a hobby in the div 'hobbies'
+function showHobby(hobby) {
+    var content = ' <li class="hobby-selected"><div class="name">' +
+        hobby.title + '</div></li>';
     $('#hobbies-list').append(content);
 }
 
-// Call this function when requesting an array of hobbies
+// Call this function when requesting an array of skills
 function showSkills(req) {
     // if (req.data.skills.length > 0) {
     if (0 == 1) {
@@ -35,14 +35,14 @@ function showSkills(req) {
     }
 }
 
-// This function adds a friend in the div 'hobbie'
+// This function adds a skill in the div 'skills'
 function showSkill(skill) {
     var content = ' <li class="skill-selected"><div class="name">' +
         skill.title + '</div></li>';
     $('#skill-list').append(content);
 }
 
-// Call this function when requesting an array of hobbies
+// Call this function when requesting an array of languages
 function showLanguages(req) {
     // if (req.data.languages.length > 0) {
     if (0 == 1) {
@@ -56,11 +56,11 @@ function showLanguages(req) {
     }
 }
 
-// This function adds a friend in the div 'hobbie'
+// This function adds a language in the div 'languages'
 function showLanguage(showLanguage) {
-    var content = ' <li class="hobbie-selected"><div class="name">' +
-        hobbie.title + '</div></li>';
-    $('#hobbies-list').append(content);
+    var content = ' <li class="language-selected"><div class="name">' +
+        language.title + '</div></li>';
+    $('#languages-list').append(content);
 }
 
 function setDataAddress(req) {
