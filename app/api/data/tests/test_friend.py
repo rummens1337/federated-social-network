8f0e6361df8b27d80773370f97b32b4d57451b1c
Michel Rummens <michelrummens@hotmail.com>
2020-06-22 20:12:03 +0200

    add sample function for tests

--- app/api/data/tests/test_friend.py
+++ app/api/data/tests/test_friend.py
@@ -3,16 +3,18 @@ from app.api.utils import good_json_response, bad_json_response
 import requests
 
 # blueprint.route('/add', methods=['POST'])
-def test_register():
-	url = 'http://localhost:5000/json'    
-    resp = requests.get(url)           
-    assert resp.status_code == 200
-    assert resp.json()["code"] == 1
-    print(resp.text)
+def test_register(self):
+	pass
 
 # blueprint.route('/delete', methods=['POST'])
 def test_delete():
     pass
+
+def f():
+    return 3
+
+def test_function():
+    assert f() == 4
 
 __all__ = ('blueprint')
 
