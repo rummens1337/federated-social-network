7d204a86b9e925cf3536945e2a60d4f386fe70d8
Michel Rummens <michelrummens@hotmail.com>
2020-06-23 19:57:15 +0200

    test

--- app/api/data/tests/text_example.py
+++ app/api/data/tests/text_example.py
@@ -1,8 +1,14 @@
 
+import pytest
+
 # This would be the function imported from another file
 def f():
     return 3
 
 # This would be the test.
-def test_function():
+def test_function_false():
     assert f() == 4
+
+# This would be the test.
+def test_function_true():
+    assert f() == 3
