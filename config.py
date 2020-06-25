757a0b04bf3572a7dcf7bed8c0ba3ce09ceb2fb1
@11322195 <basvdbrink1998@gmail.com>
2020-06-25 08:58:53 +0200

    edited add friend button

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
-# Set the following two lines.
-# If u change them later remove the 
-# mysql docker volumes (make rm) and restart (make run).
-
+# MYSQL
 MYSQL_DATABASE_USER = 'user'
 MYSQL_DATABASE_PASSWORD = 'password'
 MYSQL_DATABASE_DB = 'db'
 MYSQL_DATABASE_HOST = 'mysql'
+
+# IP
+CENTRAL_IP = "http://192.168.0.191:5000"
+
+# Application's secret
+SECRET_KEY = 'super-secret'
+
+# EMAIL
+EMAIL_ADDRESS = "therealfednet@gmail.com"
+EMAIL_PASSWORD = "fednetbest1"
+EMAIL_SECRET = "So-super-Secret-Icant!2BelieveiIt"
+EMAIL_SALT = "saltyyyfednet"
